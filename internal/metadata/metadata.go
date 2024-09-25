81ad42a7875c2c2f144a1e503ffde5077f869d44
Matias Barzaghi <matiasgbarzaghi@gmail.com>
2024-09-25 10:25:26 -0300

    feat(metadata): Added constants and helper functions

--- internal/metadata/metadata.go
+++ internal/metadata/metadata.go
@@ -1,1 +1,39 @@
 package metadata
+
+type CriteriaType string
+
+type Tags struct {
+	title       string
+	artist      string
+	albumArtist string
+	raw         map[string]string
+}
+
+type Metadata interface {
+	Title() string
+	Artist() string
+	AlbumArtist() string
+	Raw() map[string]interface{}
+}
+
+func (a *Tags) Title() string {
+	return a.raw["title"]
+}
+
+func (a *Tags) AlbumArtist() string {
+	return a.raw["albumArtist"]
+}
+
+func (a *Tags) Artist() string {
+	return a.raw["artist"]
+}
+
+func (a *Tags) Raw() map[string]string {
+	return a.raw
+}
+
+var criteriaList []CriteriaType = []CriteriaType{"albumartist", "artist", "title"}
+
+func IsValidCriteria(str string) bool {
+	return slices.Contains(criteriaList, CriteriaType(strings.ToLower(str)))
+}
