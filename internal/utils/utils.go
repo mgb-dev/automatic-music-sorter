c4bdd2df0c586a7f9e422ebb8d3fb59d24e22afe
Matias Barzaghi <matiasgbarzaghi@gmail.com>
2024-11-11 06:42:14 -0300

    feat(utils): Added DirList struct
    Helps with the directories list for the file sorter as a space optimizer

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"slices"
 	"strings"
 )
 
@@ -8,4 +9,28 @@ func NormalizeDirName(str string) string {
 	l := strings.ToLower(str)
 	res := strings.ReplaceAll(l, " ", "-")
 	return res
+}
+
+type DirList struct {
+	list []string
+}
+
+func NewDirList() *DirList {
+	return &DirList{list: make([]string, 0)}
+}
+
+func (ds *DirList) Exists(dir string) bool {
+	if len(ds.list) < 1 {
+		return false
+	}
+	return slices.Contains(ds.list, dir)
+}
+
+func (ds *DirList) Add(newDir string) {
+	dsRef := ds.list
+	if ds.Exists(newDir) {
+		return
+	}
+	ds.list = append(dsRef, newDir)
+	return
 }
